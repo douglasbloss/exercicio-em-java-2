eb73deb536a999f941c496c8482bdf72ce4c0b1f
Douglas <douglasbloss@gmail.com>
2022-08-01 22:52:58 -0300

    Exercicio2 atualizado

--- ExercicioNotaAluno/src/Aluno.java
+++ ExercicioNotaAluno/src/Aluno.java
@@ -1,10 +1,12 @@
+import java.text.DecimalFormat;
+
 public class Aluno {
     String nome;
     double nota1;
     double nota2;
     double nota3;
 
-
+    DecimalFormat df = new DecimalFormat("#,###.00");
     public Aluno(String nome) {
         this.nome = nome;
     }
@@ -13,17 +15,19 @@ public class Aluno {
 
         switch (nrProva) {
             case 1:
-            case 2:
+
                 if(nota > 30) {
                     System.out.println("Erro! Nota maior do que o máximo! (30)");
                 }else {
-                    if(nota1 == 0) {
-                        nota1 = nota;
-                    }else {
-                        nota2 = nota;
-                    }
+                    this.nota1 = nota;
                 }
                 break;
+            case 2:
+                if(nota > 30) {
+                    System.out.println("Erro Nota maior do que o máximo! (30)");
+                }else {
+                    this.nota2 = nota;
+                }
             case 3:
                 if(nota > 40) {
                     System.out.println("Erro! Nota maior do que o máximo! (40)");
@@ -33,8 +37,39 @@ public class Aluno {
                 break;
         }
     }
+    public String toString() {
+        return "Nome do aluno: " + this.nome
+                + "\n"
+                + "Média do aluno: " + df.format(this.returnaMedia())
+                + "\n"
+                + "Menor nota do aluno: " + df.format(this.retornaMenorNota())
+                + "\n"
+                + "Maior nota do aluno: " + df.format(this.retornaMaiorNota());
+    }
 
+    public double returnaMedia() {
+        return (nota1 + nota2 + nota3) / 3;
+    }
 
+    public double retornaMenorNota() {
+        if(this.nota1 < this.nota2 && this.nota1 < this.nota3) {
+            return this.nota1;
+        } else if (this.nota2 < this.nota1 && this.nota2 < this.nota3) {
+            return this.nota2;
+        } else if (this.nota3 < this.nota1 && this.nota3 < this.nota2) {
+            return this.nota3;
+        }
+        return 0;
+    }
 
-
+    public double retornaMaiorNota() {
+        if(this.nota1 > this.nota2 && this.nota1 > this.nota3) {
+            return this.nota1;
+        } else if (this.nota2 > this.nota1 && this.nota2 > this.nota3) {
+            return this.nota2;
+        } else if (this.nota3 > this.nota1 && this.nota3 > this.nota2) {
+            return this.nota3;
+        }
+        return 0;
+    }
 }
--- ExercicioNotaAluno/src/Main.java
+++ ExercicioNotaAluno/src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        Aluno aluno01 = new Aluno("Maria");
+        aluno01.adicionarNota(25,1);
+        aluno01.adicionarNota(30,2);
+        aluno01.adicionarNota(20,3);
 
+        System.out.println(aluno01);
+
+        System.out.println();
+
+        Aluno aluno02 = new Aluno("Valéria");
+        aluno02.adicionarNota(30, 1);
+        aluno02.adicionarNota(40,3);
+
+        System.out.println(aluno02);
+    }
 
 }
